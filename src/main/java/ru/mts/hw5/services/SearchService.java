b9a5409e1a7588bd4184305523323add43878f2a
VadimTolstov <tolstov50@mail.ru>
2024-03-13 22:15:55 +0300

    добавлен SearchServiceTest

--- src/main/java/ru/mts/hw5/services/SearchService.java
+++ src/main/java/ru/mts/hw5/services/SearchService.java
@@ -6,14 +6,19 @@ import ru.mts.hw5.exception.InvalidAnimalException;
 public interface SearchService {
 
     default void checkLeapYearAnimal(AbstractAnimal animal) throws InvalidAnimalException {
+        try {
+            if (animal == null) {
+                throw new InvalidAnimalException();
+            }
+        } catch (InvalidAnimalException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
         if (animal.getBirthDate().isLeapYear()) {
             System.out.println(animal.getName() + " был(а) рожден(а) в високосный год");
         }
         if (!animal.getBirthDate().isLeapYear()) {
             System.out.println(animal.getName() + " не был(а) рожден(а) в високосный год");
-        }
-        if (animal == null) {
-            throw new InvalidAnimalException();
         }
     }
 }
