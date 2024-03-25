95c7e1b551f8fb56494afe3a797f0b2f0f0031fb
VadimTolstov <tolstov50@mail.ru>
2024-03-25 19:19:41 +0300

    hw_7

--- src/main/java/ru/mts/hw5/services/AnimalRepository.java
+++ src/main/java/ru/mts/hw5/services/AnimalRepository.java
@@ -1,11 +1,14 @@
 package ru.mts.hw5.services;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import ru.mts.hw5.AbstractAnimal;
 import ru.mts.hw5.Animal;
 
 import java.time.LocalDate;
 import java.util.*;
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
 
 public interface AnimalRepository {
     default Map<String, LocalDate> findLeapYearNames(Animal[] animals) {
@@ -25,30 +28,27 @@ public interface AnimalRepository {
         Map<Animal, Integer> findOlderAnimalMap = new HashMap<>();
         if (ArrayUtils.isNotEmpty(animals)) {
             for (int i = 0; i < animals.length; i++) {
-                if (((AbstractAnimal) animals[i]).getBirthDate().getYear() < years) {
-                    findOlderAnimalMap.put(animals[i], ((AbstractAnimal) animals[i]).getBirthDate().getYear());
+                if (LocalDate.now().getYear() - ((AbstractAnimal) animals[i]).getBirthDate().getYear() < years) {
+                    findOlderAnimalMap.put(animals[i], LocalDate.now().getYear() - ((AbstractAnimal) animals[i]).getBirthDate().getYear());
                 }
             }
 
             if (findOlderAnimalMap.isEmpty()) {
-                AbstractAnimal[] array = new AbstractAnimal[animals.length];
-                for (int i = 0; i < animals.length; i++) {
-                    array[i] = (AbstractAnimal) animals[i];
-                }
+                AbstractAnimal[] array = animalCasting(animals);
                 Arrays.sort(array, new Comparator<AbstractAnimal>() {
                     @Override
                     public int compare(AbstractAnimal o1, AbstractAnimal o2) {
                         return o2.getBirthDate().getYear() - o1.getBirthDate().getYear();
                     }
                 });
-                findOlderAnimalMap.put(array[array.length - 1], array[array.length - 1].getBirthDate().getYear());
+                findOlderAnimalMap.put(array[array.length - 1], LocalDate.now().getYear() - array[array.length - 1].getBirthDate().getYear());
             }
         }
         return findOlderAnimalMap;
     }
 
-    default Map<String, Integer> findDuplicate(Animal[] animals) {
-        Map<String, Integer> findOlderAnimalMap = new HashMap<>();
+    default Map<String, List<Animal>> findDuplicate(Animal[] animals) {
+        Map<String, List<Animal>> findOlderAnimalMap = new HashMap<>();
         if (ArrayUtils.isNotEmpty(animals)) {
             boolean isListWasNotFound = true;
             List<List<Animal>> listAnimals = new ArrayList<>();
@@ -75,11 +75,52 @@ public interface AnimalRepository {
             }
 
             for (int i = 0; i < listAnimals.size(); i++) {
-                findOlderAnimalMap.put(listAnimals.get(i).get(0).getClass().getSimpleName(), listAnimals.get(i).size());
+                findOlderAnimalMap.put(listAnimals.get(i).get(0).getClass().getSimpleName(), listAnimals.get(i));
             }
 
             System.out.println(findOlderAnimalMap);
         }
         return findOlderAnimalMap;
+    }
+
+    default AbstractAnimal[] animalCasting(Animal[] animals) {
+        AbstractAnimal[] array = new AbstractAnimal[animals.length];
+        for (int i = 0; i < animals.length; i++) {
+            array[i] = (AbstractAnimal) animals[i];
+        }
+        return array;
+    }
+
+    default void findAverageAge(List<AbstractAnimal> animals) {
+        if (CollectionUtils.isNotEmpty(animals)) {
+            animals.stream()
+                    .mapToInt(animal -> LocalDate.now().getYear() - animal.getBirthDate().getYear())
+                    .average()
+                    .stream().forEach(System.out::println);
+        }
+    }
+
+    default List<AbstractAnimal> findOldAndExpensive(List<AbstractAnimal> animals) {
+        double coll = animals.stream()
+                .mapToDouble(AbstractAnimal::getCost)
+                .average()
+                .orElseGet(() -> {
+                    System.out.println("В findOldAndExpensive не смогли посчитать среднею стоимость, берем 0");
+                    return 0;
+                });
+        return animals.stream()
+                .sorted(Comparator.comparingInt(animal -> animal.getBirthDate().getYear()))
+                .filter(animal -> ((LocalDate.now().getYear() - animal.getBirthDate().getYear()) > 5) && (animal.getCost() > coll))
+                .toList();
+
+    }
+
+    default List<String> findMinConstAnimals(List<AbstractAnimal> animals) {
+          return animals.stream()
+                .sorted((o1, o2) -> o1.getCost().compareTo(o2.getCost()))
+                .limit(3)
+                .sorted((o1, o2) -> -o1.getName().compareTo(o2.getName()))
+                .map(AbstractAnimal::getName)
+                .toList();
     }
 }
--- src/test/java/ru/mts/hw5/services/AnimalsRepositoryImplTest.java
+++ src/test/java/ru/mts/hw5/services/AnimalsRepositoryImplTest.java
@@ -3,62 +3,41 @@ package ru.mts.hw5.services;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import ru.mts.hw5.AbstractAnimal;
 import ru.mts.hw5.Animal;
 import ru.mts.hw5.pets.Cat;
 import ru.mts.hw5.pets.Dog;
 import ru.mts.hw5.predators.Wolf;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
-class AnimalRepositoryTest {
-    @Mock
-    AnimalRepository animalRepositoryMoc;
+class AnimalsRepositoryImplTest {
     private Animal[] animals;
     Animal[] animalNull = null;
-
-    List<AbstractAnimal> abstractAnimalList;
-    private final AnimalRepository animalRepository = new AnimalsRepositoryImpl();
+    private final AnimalsRepositoryImpl animalRepository = new AnimalsRepositoryImpl();
 
     @BeforeEach
     void setUp() {
-        abstractAnimalList = new ArrayList<>();
         animals = new Animal[20];
         for (int i = 0; i < 10; i++) {
-            Dog dog = Dog.builder()
+            animals[i] = Dog.builder()
                     .breed("Немецкая")
                     .name("Рекс" + i)
-                    .cost(77.21 + i)
+                    .cost(77.21)
                     .character("Злюка")
                     .birthDate(LocalDate.of(2000, 3, 13 + i))
                     .build();
-
-            animals[i] = dog;
-
-            abstractAnimalList.add(dog);
         }
-
         for (int i = 10; i < 20; i++) {
-            Wolf wolf = Wolf.builder()
+            animals[i] = Wolf.builder()
                     .breed("Лесной")
                     .name("Бим" + i)
-                    .cost(88.21 + i)
+                    .cost(88.21)
                     .character("Добрый")
-                    .birthDate(LocalDate.of(1990, 10, 1 + i))
+                    .birthDate(LocalDate.of(1997, 10, 1 + i))
                     .build();
-
-            animals[i] = wolf;
-
-            abstractAnimalList.add(wolf);
         }
     }
 
@@ -96,12 +75,11 @@ class AnimalRepositoryTest {
                 .name("Бим")
                 .cost(88.21)
                 .character("Добрый")
-                .birthDate(LocalDate.of(1988, 10, 1))
+                .birthDate(LocalDate.of(1991, 10, 1))
                 .build();
         animals[0] = cat;
         assertEquals(1, animalRepository.findOlderAnimal(animals, 100).size());
-        assertEquals(36, animalRepository.findOlderAnimal(animals, 100).get(cat));
-        int i = 0;
+        assertEquals(33, animalRepository.findOlderAnimal(animals, 100).get(cat));
         assertTrue(animalRepository.findOlderAnimal(animals, 100).containsKey(cat));
     }
 
@@ -145,83 +123,5 @@ class AnimalRepositoryTest {
         assertEquals(1, animalRepository.findDuplicate(animals).get("Cat").size());
         assertEquals(3, animalRepository.findDuplicate(animals).size());
         assertTrue(animalRepository.findDuplicate(animals).containsKey("Cat"));
-    }
-
-    @Test
-    @DisplayName("Проверяем что вернется 3 объекта")
-    void findMinConstAnimalsSize() {
-        assertEquals(3, animalRepository.findMinConstAnimals(abstractAnimalList).size());
-    }
-
-    @Test
-    @DisplayName("Проверяем что вернется 3 животного отсортированный в обратном порядке и с самой низкой ценой")
-    void findMinConstAnimalsSorted() {
-        assertEquals("Рекс2", animalRepository.findMinConstAnimals(abstractAnimalList).get(0));
-        assertEquals("Рекс1", animalRepository.findMinConstAnimals(abstractAnimalList).get(1));
-        assertEquals("Рекс0", animalRepository.findMinConstAnimals(abstractAnimalList).get(2));
-
-        abstractAnimalList.add(Cat.builder()
-                .breed("Лесной")
-                .name("Альфа")
-                .cost(0.00)
-                .character("Добрый")
-                .birthDate(LocalDate.of(1991, 10, 1))
-                .build());
-        assertEquals("Альфа", animalRepository.findMinConstAnimals(abstractAnimalList).get(2));
-    }
-
-    @Test
-    @DisplayName("Проверяем что вернется 10 объектов")
-    void findOldAndExpensiveSize() {
-        assertEquals(10, animalRepository.findOldAndExpensive(abstractAnimalList).size());
-
-    }
-
-    @Test
-    @DisplayName("Проверяем что список отсортировано по возрастанию даты")
-    void findOldAndExpensiveSortedDate() {
-        assertEquals(LocalDate.of(1990, 10, 11)
-                , animalRepository.findOldAndExpensive(abstractAnimalList).get(0).getBirthDate());
-
-        assertEquals(LocalDate.of(1990, 10, 20)
-                , animalRepository.findOldAndExpensive(abstractAnimalList).get(9).getBirthDate());
-
-        abstractAnimalList.add(Cat.builder()
-                .breed("Лесной")
-                .name("Альфа")
-                .cost(200.00)
-                .character("Добрый")
-                .birthDate(LocalDate.of(1888, 10, 15))
-                .build());
-
-        assertEquals("Альфа", animalRepository.findOldAndExpensive(abstractAnimalList).get(0).getName());
-    }
-
-    @Test
-    @DisplayName("Проверяем что списке нет младше 6 лет")
-    void findOldAndExpensiveSortedYea() {
-        abstractAnimalList.add(Cat.builder()
-                .breed("Лесной")
-                .name("Альфа")
-                .cost(200.00)
-                .character("Добрый")
-                .birthDate(LocalDate.of(2023, 10, 1))
-                .build());
-        assertFalse(animalRepository.findOldAndExpensive(abstractAnimalList).stream()
-                .anyMatch(animal -> animal.getName().equals("Альфа")));
-    }
-
-    @Test
-    @DisplayName("Проверяем что списке нет объекта меньше средней стоимости")
-    void findOldAndExpensiveSortedCost() {
-        abstractAnimalList.add(Cat.builder()
-                .breed("Лесной")
-                .name("Альфа")
-                .cost(10.00)
-                .character("Добрый")
-                .birthDate(LocalDate.of(1888, 10, 1))
-                .build());
-        assertFalse(animalRepository.findOldAndExpensive(abstractAnimalList).stream()
-                .anyMatch(animal -> animal.getName().equals("Альфа")));
     }
 }
